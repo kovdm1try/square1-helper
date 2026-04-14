ef39f21f6f4513e81a23050f4b454bd16da52f7e
Dmitry Kovtunov <kovtunov.04@icloud.com>
2026-04-14 14:45:49 +1000

    feat: add error page (#16)

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,33 @@
+import { useNavigate, useParams } from 'react-router-dom';
+
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Box, Button, Typography } from '@mui/material';
+
 import styles from './ErrorPage.module.scss';
 
 const ErrorPage = () => {
-  return <div className={styles['error']}>Error</div>;
+  const navigate = useNavigate();
+  const { err } = useParams();
+
+  return (
+    <Box className={styles.errorPageWrapper}>
+      <Box className={styles.errorPageHeader}>
+        <Button variant="text" color="primary" onClick={() => navigate('/')} className={styles.backButton}>
+          <ArrowBackIosNewIcon fontSize="large" className={styles.icon} />
+        </Button>
+      </Box>
+      <Box className={styles.content}>
+        <Box className={styles.message}>
+          <Typography component="div" className={styles.errorNumber}>
+            {err}
+          </Typography>
+          <Typography component="div" className={styles.description}>
+            Something went wrong...
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ErrorPage;
